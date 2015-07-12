a2af1785e10ffd6b2b1740f7fc3d86f37a0633dd
CemeBP <lukas@bluepanel.org>
2015-07-12 23:16:17 +0200

    added rules and fillable to category model

--- app/Category.php
+++ app/Category.php
@@ -6,8 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+    /*
+     *      $table->increments('id');
+            $table->string('category_name');
+            $table->integer('parent_id');
+            $table->boolean('status');
+    */
     //
     public function products(){
         return $this->hasMany('App\Product');
     }
+
+    protected $fillable =
+        [   'category_name',
+            'parent_id',
+            'status'
+        ];
+
+    //Rules
+    public static $rules = array(
+        'category_name' => 'required|min:2',
+        'parent_id' => 'required|numeric',
+        'status' => 'required|boolean'
+    );
 }

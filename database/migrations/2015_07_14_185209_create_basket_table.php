1f6c6908061502fec625397255fcc7b555b23fd8
SStalker <raphael@bluepanel.org>
2015-07-14 20:30:55 +0200

    Added migrations  for the basket and the m:n relationship with products.

---
+++ database/migrations/2015_07_14_185209_create_basket_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasketTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baskets', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('total_price');
+            $table->integer('total_quantity');
+            $table->date('purchaseDate')->nullable();
+            $table->boolean('active');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('baskets');
+    }
+}
---
+++ database/migrations/2015_07_14_191344_create_basket_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasketProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('basket_product', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('basket_id')->unsigned()->index();
+            $table->foreign('basket_id')->references('id')->on('baskets')->onDelete('cascade');
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('basket_product');
+    }
+}

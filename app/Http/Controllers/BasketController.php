3d5355a3918188bcdf31f7732aa4ef9f0d10b8d6
SStalker <raphael@bluepanel.org>
2015-07-22 22:21:00 +0200

    Added functions for adding and deleting items in the basket + A simple view for it.

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -8,19 +8,60 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Session;
 use App\Basket;
+use App\Product;
 
 class BasketController extends Controller
 {
+    private $id;
+
 	public function __construct()
 	{
 		$this->middleware('basket');
+        $this->id = Session::get('basket_id');
 	}
 
     public function getIndex()
-    {	$basket_id = Session::get('basket_id');
-    	$products = Basket::findOrFail($basket_id)->products;
+    {
+        $basket = Basket::findOrFail($this->id);
+        $products = $basket->products;
 
-    	return view('baskets.index')
-    			->with('products', $products);
+            return view('baskets.index')
+                ->with('basket', $basket)
+                ->with('products', $products);
+    }
+
+    public function postAddProduct($product_id)
+    {
+        // Update a already inserted product
+        // Example: User::find(1)->roles()->updateExistingPivot($roleId, $attributes);
+
+        $product = Product::findOrFail($product_id);
+        $basket = Basket::findOrFail($this->id);
+
+        if($basket->products()->find($product_id)) {
+
+            //dd('This product is already in the basket. Update it.');
+            $basket->products()->updateExistingPivot($product_id, ['quantity' => $product->quantity, 'price' => $product->price]);
+
+        } else{
+
+            //dd('This product is not in the basket. Simply add it.');
+            $basket->products()->attach($product_id, ['quantity' => $product->quantity, 'price' => $product->price]);
+        }
+
+        return redirect('baskets/index');
+    }
+
+    public function postOrder()
+    {
+        // todo
+    }
+
+    public function postDeleteProduct($product_id)
+    {
+        $basket = Basket::findOrFail($this->id);
+        $basket->products()->detach($product_id);
+
+        return redirect('baskets/index');
     }
 }
--- resources/views/baskets/index.blade.php
+++ resources/views/baskets/index.blade.php
@@ -1,11 +1,44 @@
 @extends('app')
 
 @section('content')
-	@foreach($products as $product)
-		{!! $product->name !!}
-	@endforeach
-
 	@if($products->isEmpty())
 		Sie haben keine Produkte im Warenkorb
+	@else
+		<table class="table table-striped table-bordered table-hover">
+			<thead>
+				<th></th>
+				<th></th>
+				<th>Preis</th>
+				<th>Menge</th>
+				<th>Aktion</th>
+			</thead>
+			@foreach($products as $product)
+				<tr>
+					<td>{!! $product->image_path !!}</td>
+					<td>{!! $product->name !!}</td>
+					<td>{!! $product->price !!}</td>
+					<td>{!! $product->quantity !!}</td>
+					<td>
+						<div class="btn-group">
+						{!! Form::open(['method' => 'POST', 'url' => ['baskets/delete-product/'. $product->id]]) !!}
+							<button type='submit'><span class="glyphicon glyphicon-remove"></span></button>
+						{!! Form::close() !!}
+						</div>
+					</td>
+				</tr>
+			@endforeach
+		</table>
+
+		<div class="row">
+			<div class="col-md-2">
+				Summe: {!! $basket->total_price !!} €
+			</div>
+			<div class="col-md-8"></div>
+			<div class="col-md-2">
+				{!! Form::open(['method' => 'POST', 'route' => ['products.destroy', $product->id], 'class' => 'pull-right']) !!}
+					{!! Form::submit('Zur Kasse', ['class' => 'btn btn-danger']) !!}
+				{!! Form::close() !!}
+			</div>
+		</div>
 	@endif
 @stop

29e6220921591b68b35d5c2cc2cecbb30c10cccd
CemeBP <lukas@bluepanel.org>
2015-08-15 22:50:49 +0200

    Designed new frontpage

--- app/Http/Controllers/StartController.php
+++ app/Http/Controllers/StartController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Category;
+use App\Product;
 
 class StartController extends Controller
 {
@@ -18,7 +19,17 @@ class StartController extends Controller
     public function getIndex()
     {    	
     	$categories = Category::all();
+
+        //Get the 6 most ordered products
+        $productsAsc = Product::orderBy('times_ordered','asc')->get();
+        $products = array();
+        for($i = 0; $i < 6; $i++ ){
+            $products[] = $productsAsc[$i];
+        }
+
+        //return all categories for side menu and 6 most ordered products
     	return view('index')
-    		->with('categories', $categories);
+    		->with('categories', $categories)
+            ->with('products', $products);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,25 @@
 @stop
 
 @section('content')
-<h1>Startseite</h1>
+    <div class="frontpageDist row">
+        <div class="col-md-4"></div>
+        <h1 class="col-md-3 text-center">Startseite</h1>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            @foreach($products as $product)
+                <div class="frontpageDist panel col-md-3">
+                    <a href="{!! url( 'images/' .$product->id) !!}">
+                        <img class="thumbnail center-block" src=" {!! url( 'images/' .$product->image_path) !!}" alt="No image available">
+                        <div class="text-center">
+                            {!! $product->name !!}
+                        </div>
+                    </a>
+                </div>
+                <div class="frontpageDist col-md-1">
+
+                </div>
+            @endforeach
+        </div>
+    </div>
 @stop

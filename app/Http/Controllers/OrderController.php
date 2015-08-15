0f83bd902f3b3bb2077c520ba4a8a3b033cca925
SStalker <raphael@bluepanel.org>
2015-08-15 22:24:17 +0200

    Added functions for the order transaction in OrdersController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,14 +10,127 @@ use App\Order;
 use App\Basket;
 use Auth;
 use Redirect;
+use Session;
+use Validator;
 
 class OrderController extends Controller
 {
   private $custom_errors;
-  
+  private $addresses;
+  private $order;
+
+  /**
+   * Initalize the middleware
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+
+    if(Auth::check()){
+      $this->addresses = Auth::user()->addresses;
+      $this->postTransaction(Session::get('basket_id'));
+    }
+  }
+
   public function getIndex()
   {
       //
+  }
+
+  public function getCurrentBasket()
+  {
+    return Basket::findOrFail( Session::get('basket_id') );
+  }
+
+  public function getCurrentOrder()
+  {
+    $basket = $this->getCurrentBasket();
+    $order = $basket->order;
+
+    return $order;
+  }
+
+  /**
+   * Will return the chooseAddress view
+   * @return view
+   */
+  public function getChooseAddress()
+  {
+    $order = $this->getCurrentOrder();
+
+    // check for a address before
+    // cause user without one address should be redirected to create a address
+
+    return view('orders.chooseAddress')
+      ->with('addresses', $this->addresses)
+      ->with('order', $order)
+      ->with('custom_errors', $this->custom_errors);
+  }
+
+  /**
+   * Checks the input from the chooseAddress view.
+   * When invalid input it will return back with messages
+   * else it will redirect to choosePaymentMethod
+   *
+   */
+  public function postChooseAddress()
+  {
+    $request = Request::all();
+    $order = $this->getCurrentOrder();
+    
+    $validator = Validator::make($request,
+      ['optionDeliveryAddress' => 'required'],
+      ['optionBillingAddress' => 'required']
+    );
+
+    if($validator->passes())
+    {
+      $order->address_id = $request['optionDeliveryAddress'];
+
+      if($request['optionBillingAddress'] == 0)
+        $order->billing_id = $order->address_id;
+      else
+        $order->billing_id = $request['optionBillingAddress'];
+
+      $order->save();
+
+      return redirect('orders/choose-payment-method');
+    
+    }else{
+      return view('orders.chooseAddress')
+          ->with('addresses', $this->addresses)
+          ->with('order', $order)          
+          ->withErrors($validator);
+    }
+  }
+
+  public function getChoosePaymentMethod()
+  {    
+    $order = $this->getCurrentOrder();
+
+    return view('orders.choosePaymentMethod')
+          ->with('payment_methods', $this->addresses);
+  }
+
+  public function postChoosePaymentMethod()
+  {
+    $request = Request::all();    
+    // Todo;: check the input
+    //dd($request);
+    //
+    return redirect('orders/checkout');     
+  }
+
+  public function getCheckout()
+  {
+    $basket = $this->getCurrentBasket();
+    $order = $this->getCurrentOrder();
+
+    return view('orders.checkout')
+          ->with('custom_errors', $this->custom_errors)
+          ->with('order', $basket->order)
+          ->with('products', $basket->products)
+          ->with('addresses', $this->addresses); 
   }
 
   public function postTransaction($basket_id)
@@ -36,11 +149,12 @@ class OrderController extends Controller
       $order->user_id = Auth::user()->id;
       $order->basket_id = $basket->id;
       $order->save();
-
+      $this->order = $order;
       // redirect the customer to the order form site
-      return redirect('orders/check');
+      //return redirect('orders/check');
 
     } else {
+      $this->order = $basket->order;
       //dd('Ich habe bereits eine Order zugewiesen bekommen');
       // Check the order data for for nessesary data
       // First check the address of the user make the current one as standard address
@@ -48,7 +162,7 @@ class OrderController extends Controller
       // But give the customer a chance to change his address
       // Next the customer must choose a valid payment method
       // If the user got a coupon-code then validate it
-      $success = $this->check($basket_id);
+      /*$success = $this->check($basket_id);
       
       if($success)
         return view('orders/success');
@@ -57,7 +171,7 @@ class OrderController extends Controller
           ->with('custom_errors', $this->custom_errors)
           ->with('order', $basket->order)
           ->with('products', $basket->products)
-          ->with('addresses', Auth::user()->addresses);  
+          ->with('addresses', Auth::user()->addresses);  */
     }
     //Order::findOrNew($id)
     //return view('order.transaction');

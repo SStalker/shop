61f92624fdc200da0e5f21591cea67ec707c900e
CemeBP <lukas@bluepanel.org>
2015-08-18 15:57:29 +0200

    Rearranged frontpage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,26 @@
 @extends('app')
 
-@section('leftmenu')
-	@include('layouts.menuleft')
-@stop
-
 @section('content')
-    <div class="frontpageDist row">
-        <div class="col-md-4"></div>
-        <h1 class="col-md-3 text-center">Startseite</h1>
+    <div class="frontpageDist row borderBottom">
+
+        <h1 class="text-center">Startseite</h1>
     </div>
     <div class="container-fluid">
         <div class="row">
             @foreach($articles as $article)
-                <div class="frontpageDist frontpagePanel panel col-md-3">
-                    <a href="{!! url( 'articles/' .$article->id) !!}">
-                        <img class="thumbnail center-block" src=" {!! url( 'images/' .$article->image_path) !!}" alt="No image available">
-                        <div class="text-center">
-                            {!! $article->name !!}
+                <div class="frontpageDist frontpagePanel panel col-md-4">
+                    <div class="row">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8 frontpagePanelBorder">
+                            <a href="{!! url( 'articles/' .$article->id) !!}">
+                                <img class="thumbnail center-block" src=" {!! url( 'images/' .$article->image_path) !!}" alt="No image available">
+                                <div class="text-center">
+                                    {!! $article->name !!}
+                                </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                <div class="frontpageDist col-md-1">
-
+                        <div class="col-md-2"></div>
+                    </div>
                 </div>
             @endforeach
         </div>
